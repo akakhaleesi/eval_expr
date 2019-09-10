4597da68605af1b3936eeeb1f9a06ed98002c02a
Your Name <you@example.com>
2019-09-10 14:52:43 +0200

    okokok

---
+++ eval_expr.php
@@ -0,0 +1,85 @@
+<?php
+
+Class Eval_Expr {
+
+	public function __construct($expr){
+		$this->expr = $expr;
+		$this->operators = ['+','-','/','*','%','(',')'];
+		$this->numbers = ['0','1','2','3','4','5','6','7','8','9'];
+		$this->final_expr = '';
+	}
+
+	public function eval(){
+		if(!is_string($this->expr)) return "parameter must be a string\n";
+
+		$res = $this->loop($this->expr, false);
+
+		return "res: $res\n\nfinal expr: $this->final_expr\n\n";
+	}
+
+	public function calcul($expr){
+		if(strlen($expr) < 1) return "function calcul 1: syntax error\n\n";
+		if(!in_array($expr[0], $this->numbers)) return "function calcul 2: syntax error\n\n";
+		$last_n = $expr[0];
+
+		for($i=1; $i<strlen($expr);$i++){
+
+			switch($expr[$i]){
+
+				case "-":
+					if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
+					$last_n = $last_n - $expr[$i+1];
+					break;
+
+				case "+":
+					if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
+					$last_n = $last_n + $expr[$i+1];
+					break;
+
+				case "*":
+					if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
+					$last_n = $last_n * $expr[$i+1];
+					break;
+
+				default: echo "default\n\n";
+			}
+		}
+		return $last_n;
+	}
+
+	public function loop($expr, $parentheses=false){echo "$expr\n\n";
+		$current_expr = '';
+		$parentheses_expr = '';
+
+		for($i=0; $i<strlen($expr); $i++){
+
+			if(in_array($expr[$i], $this->operators) || in_array($expr[$i], $this->numbers)){
+echo "$expr[$i]\n\n";
+				if($expr[$i]==='('){echo "[1]\n\n";
+					$this->final_expr .= substr($expr, 0, $i-1);
+					$this->loop(substr($expr, $i+1), true);
+				}
+
+				else if($expr[$i]===')'){echo "[2]\n\n";
+					$parentheses = false;
+					echo "parentheses: $parentheses_expr\n\n";
+					$this->final_expr .= $this->calcul($parentheses_expr);
+					if(strlen($expr) > $i+1) $this->final_expr .= substr($expr, -1, $i+1);
+					$this->loop($this->final_expr, false);
+				}
+
+				else {echo "[3]\n\n";
+					if(!$parentheses) $this->final_expr .= $expr[$i];
+					else $parentheses_expr .= $expr[$i];
+				}
+
+			}
+			else return "function loop: syntax error\n\n";
+		}
+		return $this->calcul($this->final_expr);
+	}
+
+}
+
+$expr = new Eval_Expr("5+(5+5)");
+echo $expr->eval();
