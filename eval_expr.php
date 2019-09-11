fc1421b14a456fc3fee6af39bcda2a55b917cb33
Your Name <you@example.com>
2019-09-11 18:10:49 +0200

    multiplication first

--- eval_expr.php
+++ eval_expr.php
@@ -13,47 +13,10 @@ Class Eval_Expr {
 		if(!is_string($this->expr)) return "parameter must be a string\n";
 
 		$res = $this->loop($this->expr, false);
-		echo "res: $res\n\nfinal expr: $this->final_expr\n\n";
-	}
-
-	public function calcul($expr){
-		preg_match_all("/(\d+|-|\+|\/|\*|\%)/", $expr, $output);
-		if(!preg_match("/(\d+)/", $output[0][0])) return;
-
-		$val = $output[0][0];
-		$n = '';
-
-		for($i=1; $i<count($output[0]);$i++){
-			switch($output[0][$i]){
-				case "-":
-					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
-					$val = $val - $output[0][$i+1];
-					break;
-				case "+":
-					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
-					$val = $val + $output[0][$i+1];
-					break;
-				case "*":
-					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
-					$val = $val * $output[0][$i+1];
-					break;
-				case "/":
-					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
-					$val = $val / $output[0][$i+1];
-					break;
-				case "%":
-					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
-					$val = $val % $output[0][$i+1];
-					break;
-				default:
-					break;
-			}
-		}
-		return $val;
+		echo "\n\ncalcul: $this->final_expr\n\nresult: $res\n\n";
 	}
 
 	public function loop($expr, $parentheses=false){
-		var_dump("LOOOOOOOOOOOP");
 		$parentheses_expr = '';
 
 		for($i=0; $i<strlen($expr); $i++){
@@ -69,11 +32,9 @@ Class Eval_Expr {
 						$this->loop = false;
 						$parentheses = false;
 						$this->final_expr .= $this->calcul($parentheses_expr);
-						var_dump(strlen($this->expr), $this->stored_expr);
+
 						if(strlen($this->expr) > $this->stored_expr){
 							$this->loop = true;
-							echo "HOLLY SHIT\n\n";
-							var_dump(substr($this->expr, $this->stored_expr+1));
 							$this->loop(substr($this->expr, $this->stored_expr+1));
 							$this->loop = false;
 							$this->stored_expr = null;
@@ -97,7 +58,53 @@ Class Eval_Expr {
 		return $this->calcul($this->final_expr);
 	}
 
+	public function calcul($expr){
+		$res = '';
+		preg_match_all("/(\d+|-|\+|\/|\*|\%)/", $expr, $output);
+		if(!preg_match("/(\d+)/", $output[0][0])) return;
+		if($index = array_search('*', $output[0])){
+			var_dump($output[0], $index);
+			$res .= $this->switch([0 => $output[$index-1], 1 => $output[$index], 2 => $output[$index+1]]);
+			array_splice($output[0], $index-1, 3);
+		}
+		$res .= $this->switch($output[0]);
+		return $res;
+	}
+
+	public function switch($array){
+		$val = $array[0];
+		$n = '';
+
+		for($i=1; $i<count($array[0]);$i++){
+			switch($array[0][$i]){
+				case "-":
+					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
+					$val = $val - $array[0][$i+1];
+					break;
+				case "+":
+					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
+					$val = $val + $array[0][$i+1];
+					break;
+				case "*":
+					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
+					$val = $val * $array[0][$i+1];
+					break;
+				case "/":
+					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
+					$val = $val / $array[0][$i+1];
+					break;
+				case "%":
+					//if(strlen($expr) === $i) return "function calcul 3: syntax error\n\n";
+					$val = $val % $array[0][$i+1];
+					break;
+				default:
+					break;
+			}
+		}
+		return $val;
+	}
+
 }
 
-$expr = new Eval_Expr("5+(10+5)/2");
+$expr = new Eval_Expr("(3+2)*5");
 $expr->eval();
